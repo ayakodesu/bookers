8d599ce0678833fe3131380b82c6a578a9563e18
EC2 Default User <ec2-user@ip-172-31-8-53.ap-northeast-1.compute.internal>
2022-08-28 05:15:11 +0000

    [Add]tableタグをつけた

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,40 +2,40 @@ class BooksController < ApplicationController
   def new
     @book = Book.new
   end
-  
+
   def index
     @books = Book.all
     @book = Book.new
   end
-  
+
 def create
   @book = Book.new(book_params)
   @book.save
-  redirect_to books_path
+  redirect_to books_path(book.id)
 end
 
 def show
-    @book = Book.find(params[:book_id])
+    @book = Book.find(params[:id])
 end
-  
-  
+
+
   def update
   book = book.find(params[:id])
   book.update(book_params)
   redirect_to books_path(book.id)
-  
+
   end
-  
+
   def edit
     @book = Book.find(params[:id])
   end
-  
+
   def destroy
-    book = Book.find(params[:id])
+    book = Book.find_by(params[:id])
     book.destroy
     redirect_to '/books'
   end
-  
+
   private
   def book_params
     params.require(:book).permit(:title, :body)
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   post 'books' => 'books#create'
   get '/' => 'homes#top'
   get 'books' => 'books#index'
-  get 'books/:id' => 'books#show', as: 'show_book'
+  get 'books/:id' => 'books#show', as: 'show'
   get 'books/:id/edit' => 'books#edit', as: 'edit_book'
   patch 'books/:id' => 'books#update', as: 'update_book'
   delete 'books/:id' =>'books#destroy', as: 'destroy_book'
